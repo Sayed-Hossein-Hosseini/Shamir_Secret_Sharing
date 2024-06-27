d03bec5091935394eb4f372aa0ec91a6cababdd9
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-06-27 15:29:23 +0330

    Selection of random coefficients

--- src/ShamirSecretSharing.java
+++ src/ShamirSecretSharing.java
@@ -6,6 +6,12 @@ public class ShamirSecretSharing {
         BigInteger[] shares = new BigInteger[n];
         Random random = new Random();
 
+        // Selection of random coefficients
+        BigInteger[] coefficients = new BigInteger[t - 1];
+        for (int i = 0; i < t - 1; i++) {
+            coefficients[i] = new BigInteger(p, random);
+        }
+
 
 
         return shares;
