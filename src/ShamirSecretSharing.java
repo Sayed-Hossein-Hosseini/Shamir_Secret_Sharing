a4e74440c6962874cc99360b2036b8a736164a89
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-06-27 15:28:33 +0330

    create generateShares Method

--- src/ShamirSecretSharing.java
+++ src/ShamirSecretSharing.java
@@ -1,2 +1,13 @@
+import java.math.BigInteger;
+import java.util.Random;
+
 public class ShamirSecretSharing {
+    private static BigInteger[] generateShares(BigInteger secret, int n, int t, int p) {
+        BigInteger[] shares = new BigInteger[n];
+        Random random = new Random();
+
+
+
+        return shares;
+    }
 }
