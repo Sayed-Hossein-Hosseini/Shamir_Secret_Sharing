6d3df55b9146bc3b5eddaa64f91f99ee495f932d
Sayyed Hossein Hosseini <s.hossein.hosseini1381@gmail.com>
2024-07-02 17:20:15 +0330

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
 public class Main {
-    public static void main(String[] args) { // 5 4 153 5442 87 60 81 15 123
-        UserInterface.main();
-    }
+    public static void main(String[] args) { UserInterface.main(); }
 }
